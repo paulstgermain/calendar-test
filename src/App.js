5da85e91697975660f92d287a3291d2a2cf18e45
paulstgermain <paulg.stgermain@gmail.com>
2022-03-15 12:13:38 -0400

    creates repo, calendar component, modal functionality on cell click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
+import "antd/dist/antd.css";
+import { Calendar, Modal } from 'antd';
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const showModal = (value) => {
+    setSelectedDate(value.format("DD-MM-YYYY"));
+    setIsModalVisible(true);
+};
+
+const handleOk = () => {
+    setIsModalVisible(false);
+}
+
+const handleCancel = () => {
+    setIsModalVisible(false);
+}
+
+function getMonthData(value) {
+  if (value.month() === 3) {
+    return 1394;
+  }
+}
+
+function monthCellRender(value) {
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Backlog number</span>
+    </div>
+  ) : null;
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={ { 
+      display: 'block', width: 700, padding: 30
+    } }>
+      <h4>ReactJS Ant-Design Calendar Component</h4>
+      <Calendar monthCellRender={monthCellRender} onSelect={showModal} />
+      <Modal title="Selected Date" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>{selectedDate ? selectedDate : 'Something went wrong.'}</p>
+      </Modal>
     </div>
   );
 }
