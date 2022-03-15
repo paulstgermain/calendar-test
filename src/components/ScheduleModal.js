b7353b77c9fe270fab601b00b0652344cdffcca3
paulstgermain <paulg.stgermain@gmail.com>
2022-03-15 14:49:38 -0400

    adds form to scheduling modal

--- src/components/ScheduleModal.js
+++ src/components/ScheduleModal.js
@@ -1,22 +1,65 @@
-import React, { useState } from 'react';
-import { Modal } from 'antd';
+import React, { useState } from "react";
+import { Modal, Form, Input, Button } from "antd";
 
 export default function ScheduleModal(props) {
-    const { isModalVisible, setIsModalVisible, eventsArr, setEventsArr } = props;
+  const { isModalVisible, setIsModalVisible, eventsArr, setEventsArr } = props;
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-    }
-    
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    }
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
 
-    return (
-        <>
-            <Modal title="Schedule a Meeting" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                <p>Create Event Modal Here - Test</p>
-            </Modal>
-        </>
-    )
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      <Modal
+        title="Schedule a Meeting"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: "success" }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
 }
